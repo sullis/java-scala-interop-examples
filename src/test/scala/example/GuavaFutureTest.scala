4f7fe078c58177966573d30c771a14892c4ab8b6
Sean Sullivan <ssullivan@gilt.com>
2016-04-18 18:41:27 -0700

    add GuavaFutureTest

---
+++ src/test/scala/example/GuavaFutureTest.scala
@@ -0,0 +1,31 @@
+package example
+
+import java.util.concurrent.ExecutionException
+import com.gilt.gfc.guava.future.FutureConverters._
+import com.google.common.util.concurrent.SettableFuture
+import scala.concurrent.Future
+import org.scalatest.{ShouldMatchers, FlatSpec}
+import org.scalatest.concurrent.ScalaFutures._
+
+class GuavaFutureTest extends FlatSpec with ShouldMatchers {
+
+  "Guava ListenableFuture conversion" should "work" in {
+    val guavaFuture = SettableFuture.create[String]
+    guavaFuture.set("Hello world")
+
+    val future = guavaFuture.asScala
+    future.isCompleted should be (true)
+    future.futureValue should be ("Hello world")
+    future.asListenableFuture.get should be ("Hello world")
+
+    val failedFuture = Future.failed[String](new IllegalStateException("OMG WTF"))
+    failedFuture.isCompleted should be (true)
+
+    val failedGuavaFuture = failedFuture.asListenableFuture
+    failedGuavaFuture.isDone should be (true)
+    val cause = intercept[ExecutionException] { failedGuavaFuture.get }.getCause
+    cause.isInstanceOf[IllegalStateException] should be (true)
+    cause.getMessage should be ("OMG WTF")
+  }
+
+}
