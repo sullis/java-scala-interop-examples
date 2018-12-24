33b917f51d57405ffaa78707f7dedf701c20a083
Sean Sullivan <github@seansullivan.com>
2018-12-23 18:11:00 -0800

    cleanup [ScalaStuff.scala]

--- src/test/scala/example/ScalaStuff.scala
+++ src/test/scala/example/ScalaStuff.scala
@@ -3,11 +3,11 @@ package example
 import scala.collection.JavaConverters._
 
 object ScalaStuff {
-  def listOfPersons = List(PersonCaseClass("a", "b", "c"), PersonCaseClass("d", "e", "f"))
-  def listOfStrings: List[String] = List("Canada", "United States")
-  def javaListOfStrings: java.util.List[String] = List("Canada", "United States").asJava
-  def listOfIntegers: List[Int] = List(10, 20, 30)
-  def setOfIntegers: Set[Int] = Set (1, 2, 3)
-  def setOfStrings: List[String] = List("Sydney", "Melbourne")
-  def foodieMap: Map[String, String] = Map("New York City" -> "Bagels", "Hawaii" -> "Pineapple")
+  val listOfPersons = List(PersonCaseClass("a", "b", "c"), PersonCaseClass("d", "e", "f"))
+  val listOfStrings: List[String] = List("Canada", "United States")
+  val javaListOfStrings: java.util.List[String] = List("Canada", "United States").asJava
+  val listOfIntegers: List[Int] = List(10, 20, 30)
+  val setOfIntegers: Set[Int] = Set (1, 2, 3)
+  val setOfStrings: List[String] = List("Sydney", "Melbourne")
+  val foodieMap: Map[String, String] = Map("New York City" -> "Bagels", "Hawaii" -> "Pineapple")
 }
